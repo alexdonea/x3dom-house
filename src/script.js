0c124113a7934d1b2311361c22df1ea14bd20bf2
Alexandru Donea <alex.donea92@gmail.com>
2023-05-19 18:24:45 +0300

    Minor update to translations
    Minor updates to js functions for translations.

--- src/script.js
+++ src/script.js
@@ -51,38 +51,69 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function showRoof() {
   const roofElements = document.getElementsByClassName("roof");
-  setRoofTranslations(roofElements, 0, 890, 0, 0, 1070, 228, 0, 1070, -220, -130, 1120, -150, 598, 908, 0, 598, 988, 0, 598, 1060, 0, 598, 1140, 0, 598, 1200, 0, -598, 908, 0, -598, 988, 0, -598, 1060, 0, -598, 1140, 0, -598, 1200, 0);
+  setRoofTranslations(
+    roofElements,
+    0, 890, 0,
+    0, 1070, 228,
+    0, 1070, -220,
+    -130, 1120, -150,
+    598, 908, 0,
+    598, 1060, 0,
+    598, 1140, 0,
+    598, 1200, 0,
+    -598, 908, 0,
+    -598, 988, 0,
+    -598, 1060, 0,
+    -598, 1140, 0,
+    -598, 1200, 0
+  );
   rotateRoofElement(roofElements[3], 0.0, 1, 0, 0);
 }
 
 function hideRoof() {
-  const roofElements = document.getElementsByClassName("roof");
-  setRoofTranslations(roofElements, 0, 170, 0, 0, 380, 228, 0, 380, -220, -130, 410, -150, 598, 208, 0, 598, 288, 0, 598, 360, 0, 598, 440, 0, 598, 500, 0, -598, 208, 0, -598, 288, 0, -598, 360, 0, -598, 440, 0, -598, 500, 0);
-  rotateRoofElement(roofElements[3], 0.0, 0.0, 0, 10);
-}
-
-function setRoofTranslations(elements, ...translations) {
-  for (let i = 0; i < elements.length; i++) {
-    const translationValues = translations.slice(i * 3, (i + 1) * 3);
-    const translation = translationValues.join(" ");
-    elements[i].setAttribute("translation", translation);
+    const roofElements = document.getElementsByClassName("roof");
+    setRoofTranslations(
+      roofElements,
+      0, 170, 0,
+      0, 380, 228,
+      0, 380, -220,
+      -130, 410, -150,
+      598, 208, 0,
+      598, 288, 0,
+      598, 360, 0,
+      598, 440, 0,
+      598, 500, 0,
+      -598, 208, 0,
+      -598, 288, 0,
+      -598, 360, 0,
+      -598, 440, 0,
+      -598, 500, 0
+    );
+    rotateRoofElement(roofElements[3], 0.0, 0.0, 0, 10);
   }
-}
 
-function rotateRoofElement(element, angle, x, y, z) {
-  element.setAttribute("rotation", `${angle} ${x} ${y} ${z}`);
-}
+  function setRoofTranslations(elements, ...translations) {
+    for (let i = 0; i < elements.length; i++) {
+      const translationValues = translations.slice(i * 3, (i + 1) * 3);
+      const translation = translationValues.join(" ");
+      elements[i].setAttribute("translation", translation);
+    }
+  }
 
-function openMainDoor() {
-  const door = document.getElementsByClassName("door")[0];
+  function rotateRoofElement(element, angle, x, y, z) {
+    element.setAttribute("rotation", `${angle} ${x} ${y} ${z}`);
+  }
 
-  door.setAttribute("rotation", "0.0 1 0 30");
-  door.setAttribute("translation", "600 78 -20");
-}
+  function openMainDoor() {
+    const door = document.getElementsByClassName("door")[0];
 
-function closeMainDoor() {
-  const door = document.getElementsByClassName("door")[0];
+    door.setAttribute("rotation", "0.0 1 0 30");
+    door.setAttribute("translation", "600 78 -20");
+  }
 
-  door.setAttribute("rotation", "0.0 0.0 0 10");
-  door.setAttribute("translation", "670 78 60");
-}
+  function closeMainDoor() {
+    const door = document.getElementsByClassName("door")[0];
+
+    door.setAttribute("rotation", "0.0 0.0 0 10");
+    door.setAttribute("translation", "670 78 60");
+  }
